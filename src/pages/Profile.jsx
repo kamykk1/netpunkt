71c3d2c9767ac1d454d24539b48bdbfbf9254261
p.kamecki <p.kamecki@o2.pl>
2026-02-26 22:43:28 +0000

    File changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -138,6 +138,9 @@ export default function Profile() {
             <TabsTrigger value="avatar" className="data-[state=active]:bg-purple-600 data-[state=active]:text-white text-xs px-3"><Camera className="w-3.5 h-3.5 mr-1" /> Zdjęcie</TabsTrigger>
             <TabsTrigger value="company" className="data-[state=active]:bg-cyan-600 data-[state=active]:text-white text-xs px-3"><Building2 className="w-3.5 h-3.5 mr-1" /> Firma</TabsTrigger>
             <TabsTrigger value="history" className="data-[state=active]:bg-amber-600 data-[state=active]:text-white text-xs px-3"><History className="w-3.5 h-3.5 mr-1" /> Historia</TabsTrigger>
+            <TabsTrigger value="stats" className="data-[state=active]:bg-purple-600 data-[state=active]:text-white text-xs px-3"><Gamepad2 className="w-3.5 h-3.5 mr-1" /> Statystyki</TabsTrigger>
+            <TabsTrigger value="elo" className="data-[state=active]:bg-cyan-600 data-[state=active]:text-white text-xs px-3"><TrendingUp className="w-3.5 h-3.5 mr-1" /> ELO</TabsTrigger>
+            <TabsTrigger value="friends" className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-xs px-3"><Users className="w-3.5 h-3.5 mr-1" /> Znajomi</TabsTrigger>
             <TabsTrigger value="notif" className="data-[state=active]:bg-emerald-600 data-[state=active]:text-white text-xs px-3"><Bell className="w-3.5 h-3.5 mr-1" /> Powiadomienia</TabsTrigger>
             <TabsTrigger value="achievements" className="data-[state=active]:bg-yellow-600 data-[state=active]:text-white text-xs px-3"><Trophy className="w-3.5 h-3.5 mr-1" /> Osiągnięcia</TabsTrigger>
             <TabsTrigger value="quick" className="data-[state=active]:bg-slate-600 data-[state=active]:text-white text-xs px-3"><CreditCard className="w-3.5 h-3.5 mr-1" /> Linki</TabsTrigger>
